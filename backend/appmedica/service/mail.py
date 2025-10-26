e36e4c96d2b4ee978790eac735aa5f2f15c586a1
Jakub Zehner <kubusja.pl@gmail.com>
2025-10-26 18:28:04 +0100

    sort mails by date

--- backend/appmedica/service/mail.py
+++ backend/appmedica/service/mail.py
@@ -1,5 +1,7 @@
+from typing import cast
 from fastapi import Depends
-from sqlmodel import Session, select
+from sqlalchemy import ColumnElement
+from sqlmodel import Session, desc, select
 from appmedica.database import get_session
 from appmedica.database.mail import MailModel
 
@@ -13,6 +15,11 @@ class MailService:
         self.db = db
 
     def get_all_mails(self, skip: int, limit: int) -> list[MailModel]:
-        query = select(MailModel).offset(skip).limit(limit)
+        query = (
+            select(MailModel)
+            .order_by(desc(cast(ColumnElement, MailModel.received_at)))
+            .offset(skip)
+            .limit(limit)
+        )
         results = self.db.exec(query).all()
         return list(results)
